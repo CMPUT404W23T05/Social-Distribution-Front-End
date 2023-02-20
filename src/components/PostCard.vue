169caf56da1c7af4f357e16b9c5522a96e3d648c
jd-lo <jdlo@ualberta.ca>
2023-02-19 19:18:17 -0800

    Card renders commonmark

---
+++ src/components/PostCard.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="card" @click="goTo()">
+
+        <img :src="author.profileImage" alt="profile picture">
+
+        <div class="card-header">
+            {{ post.title }}
+        </div>
+
+        <!-- If post is an image -->
+        <img 
+        v-if="post.type === 'image'" 
+        src="post.img_link" 
+        class="card-img-top" 
+        alt="content image"/>
+
+        <!-- If post is text (incl markdown)-->
+        <div v-else class="card-body">
+            <vue-markdown :source="post.content"></vue-markdown>
+        </div>
+    </div>
+</template>
+
+<script>
+  import VueMarkdown from 'vue-markdown-render'
+
+  export default {
+    data() {
+        return {};
+    },
+    props: ['author', 'post'],
+    methods: {
+        //Dynamically route to post page on click
+        goTo() {
+            return {};
+        },
+
+        renderMarkDown() {
+
+        }
+    },
+    components: {
+        VueMarkdown
+    },
+  }  
+</script>
