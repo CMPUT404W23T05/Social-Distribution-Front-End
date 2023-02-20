38e98218dc5c39761b75f1aa6074791fa25fda4e
geofferb <38046772+geofferb@users.noreply.github.com>
2023-02-19 19:24:10 -0700

    Basic login view, no backend yet

---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Login</h1>
+  <div class="log-in">
+    <form @submit.prevent="submitForm" style="display: inline;">
+      <label>Username: <input type="text" name="username" v-model="username"></label><br>
+      <label>Password: <input type="password" name="password" v-model="password"></label><br>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LogIn',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm (e) {
+      const formData = {
+        username: this.username,
+        password: this.password
+      }
+    }
+    // TODO: Send formData to backend
+  }
+}
+
+</script>
