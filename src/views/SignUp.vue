f9faa95293a98fc75e0a53cd5a734fc2a000eb1a
geofferb <38046772+geofferb@users.noreply.github.com>
2023-02-28 23:18:41 -0700

    add confirm password and check if passwords match

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -4,6 +4,7 @@
       <form @submit.prevent="submitForm" style="display: inline;">
         <label>Username: <input type="text" name="username" v-model="username"></label><br>
         <label>Password: <input type="password" name="password" v-model="password"></label><br>
+        <label>Confirm Password: <input type="password" name="password" v-model="confirmPassword"></label><br>
         <button type="submit">Sign Up</button>
       </form>
 <br>Already have an account? <router-link to="/login">Log in</router-link>
@@ -17,7 +18,8 @@ export default {
   data () {
     return {
       username: '',
-      password: ''
+      password: '',
+      confirmPassword: ''
     }
   },
   methods: {
@@ -26,6 +28,12 @@ export default {
       const formData = {
         username: this.username,
         password: this.password
+      }
+      if (this.password !== this.confirmPassword) {
+        alert('Passwords do not match')
+        // clear the password fields
+        this.password = ''
+        this.confirmPassword = ''
       }
       // TODO: Send formData to backend
       axios
