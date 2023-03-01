801116b543e9369abce66fa20cb1ec8826c360e7
geofferb <38046772+geofferb@users.noreply.github.com>
2023-02-28 18:23:10 -0700

    add baseurl and activate axios in main.js

--- src/main.js
+++ src/main.js
@@ -4,5 +4,8 @@ import { createPinia } from 'pinia'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
+import axios from 'axios'
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+axios.defaults.baseURL = 'http://localhost:8000/api' // API URL
+
+createApp(App).use(router, axios).use(createPinia()).mount('#app')
