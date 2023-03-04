3f72a4dfa05cb0e222f933c6bc674ce6c8e07871
geofferb <38046772+geofferb@users.noreply.github.com>
2023-03-04 13:14:48 -0700

    Require auth for all pages except login and signup

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import routes from './routes.js'
-
+import { useTokenStore } from '@/stores/token.js'
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   linkActiveClass: 'active',
   routes
 })
-
+router.beforeEach((to, from) => {
+  const token = useTokenStore()
+  // if the user is not logged in and tries to access a page other than login or signup, redirect to login
+  const exceptions = ['login', 'signup']
+  if (!token.isAuthenticated && !exceptions.includes(to.name)) {
+    return '/login'
+  }
+})
 export default router
