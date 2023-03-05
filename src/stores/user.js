a60a03ca59cf0854de8e48f7909e70886f854c24
geofferb <38046772+geofferb@users.noreply.github.com>
2023-03-05 16:34:32 -0700

    Ensure user object in store is not a string

--- src/stores/user.js
+++ src/stores/user.js
@@ -6,13 +6,14 @@ export const useUserStore = defineStore('user', {
   }),
   actions: {
     initializeStore () {
-      if (localStorage.getItem('user')) { // if there is a user in local storage
-        this.user = localStorage.getItem('user')
+      if (localStorage.getItem('user')) { // if there is a user in local
+        this.user = JSON.parse(localStorage.getItem('user'))
       } else {
         this.user = {}
       }
     },
     setUser (user) { // set the user in local storage when the user logs in
+      console.assert(typeof user === 'object', 'user must be an object')
       this.user = user
     },
     removeUser () { // remove the user from local storage when the user logs out
