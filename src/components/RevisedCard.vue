9f73e131408a59e06d10155813b5f4c933050b5c
jd-lo <jdlo@ualberta.ca>
2023-03-17 14:27:34 -0600

    Clean-up and basic card styling in progress

--- src/components/RevisedCard.vue
+++ src/components/RevisedCard.vue
@@ -1,44 +1,48 @@
 <template>
-  <div
-    class="card-body card"
-    @mouseover="hovered = true"
-    @mouseleave="hovered = false"
-  >
+  <card-totality>
     <div class="post-title">
-      <h3 class="title-text">{{ post.title }}</h3>
+        <h3 class="title-text">{{ post.title }}</h3>
     </div>
-    <div class="content-container">
-      <vue-markdown
-        v-if="markdownEnabled"
-        class="text-content"
-        :source="post.content"
-      ></vue-markdown>
-      <!-- Post is plain text -->
-      <p v-else-if="post.content" class="text-content">{{ post.content }}</p>
-      <img
-        class="image-content"
-        v-if="post.image"
-        :src="imageURL"
-        :alt="post.description"
-      />
-    </div>
-    <div :class="{ open: hovered }" class="footer card-footer text-muted">
-      <slot name="footer">
+    <div
+      class="card-body card"
+      @mouseover="hovered = true"
+      @mouseleave="hovered = false"
+    >
+      <div class="content-container">
+        <div class="decoration-accent"></div>
+        <div v-if="hovered" class="scrim"></div>
+        <vue-markdown
+          v-if="markdownEnabled"
+          class="text-content"
+          :source="post.content"
+        ></vue-markdown>
+        <!-- Post is plain text -->
+        <p v-else-if="post.content" class="text-content">{{ post.content }}</p>
         <img
-          class="profile-image"
-          :src="author.profileImage"
-          :alt="author.profileName"
+          class="image-content"
+          v-if="post.image"
+          :src="imageURL"
+          :alt="post.description"
         />
-        <h6 id="username">@{{ author.displayName }}</h6>
-        <span
-          class="content-types"
-          v-for="content in post.ContentType"
-          :key="content"
-          >{{ content }}
-        </span>
-      </slot>
+      </div>
+      <div :class="{ open: hovered }" class="footer card-footer text-muted">
+        <slot name="footer">
+          <img
+            class="profile-image"
+            :src="author.profileImage"
+            :alt="author.profileName"
+          />
+          <h6 id="username">@{{ author.displayName }}</h6>
+          <span
+            class="content-types"
+            v-for="content in post.ContentType"
+            :key="content"
+            >{{ content }}
+          </span>
+        </slot>
+      </div>
     </div>
-  </div>
+  </card-totality>
 </template>
 
 <script>
@@ -69,72 +73,104 @@ export default {
     font-size: 16pt;
     white-space: nowrap;
     overflow: hidden;
-    color: white;
-    background-color: rgb(57, 57, 57);
+    color: #2c2c2c;
+    font-weight: bold;
+    text-transform: uppercase;
   }
   .title-text {
-    padding: 1% 1% 1% 1%;
     font-size: large;
   }
   .content-container {
     display: flex;
-    padding: 2% 2% 2% 2%;
-    height: 140pt;
+    height: 100%;
+    padding: 0;
+    margin: 0.5em 0.8em;
     text-align: left;
     align-items: center;
     justify-content: space-evenly;
+    box-sizing: content-box;
+  }
+
+  .scrim {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(rgba(0,0,0,0), rgba(0, 0, 0, 0.3));
+  }
+
+  .decoration-accent {
+    position: absolute;
+    border-right: 2pt solid #4998F5;
+    border-top: 2pt solid #4998F5;
+    width: 90%;
+    height: calc(100% + 28pt);
+    top: -28pt;
+    right: 0.8em;
   }
 
   .image-content {
-    width: auto;
-    height: auto;
     max-width: 100%;
-    max-height: 100%;
-    object-fit: contain;
+    min-height: 100%;
+    object-fit: cover;
+    flex: 0 0 50%;
   }
 
   .text-content {
     word-wrap: break-word;
     text-overflow: ellipsis;
     overflow: hidden;
+    flex: 0 0 50%;
   }
 
   .footer {
     display: none;
+    float: bottom;
     position: absolute;
     bottom: 0;
     height: 0;
+    border: none;
+    border-radius: 0 !important;
+    background-color: rgba(70, 70, 70, 0.8);
     transition: all 1s;
   }
 
   .footer.open {
     display: flex;
-    height: 25%;
+    height: 20%;
     width: 100%;
-    background-color: rgba(0, 0, 0, 0.25);
+    color: #FFF !important;
     margin-left: 0;
     align-items: center;
   }
 
   .profile-image {
-    height: 100%;
-    aspect-ratio: 1/1;
-    border-radius: 50pt;
-    object-fit: scale-down;
+    height: 5em;
+    width: 5em;
+    border-radius: 50%;
+    object-fit: cover;
   }
 
   .card {
     /* width: 15%; */
-    min-width: 200pt;
-    max-width: 300pt;
-    margin: 5pt;
+    min-height: 200pt;
+    min-width: 300pt;
+    margin: 0pt;
     padding: 2pt 0;
     width: 120pt;
     height: 144pt;
-    aspect-ratio: 5/6;
     max-width: 120pt;
     max-height: 144pt;
     display: flex;
-    flex-direction: vertical;
+    border: none;
+    border-radius: 0;
+
+    background-color: #fafafa;
+    box-shadow: 0.1rem 0.25rem 0.5rem rgba(0,0,0,0.25);
   }
+
+  img {
+    margin: 0;
+    flex-basis: 1;
+  }
+
 </style>
