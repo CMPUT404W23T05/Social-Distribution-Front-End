9a082c42def5f89b00e1cece857930ab3ebace34
Elena Xu <toppingart123@gmail.com>
2023-03-20 07:47:16 -0600

    modifying social components to fit restructuring

--- src/components/socialComponents/MyFollowees.vue
+++ src/components/socialComponents/MyFollowees.vue
@@ -1,75 +1,75 @@
 <template>
-<div class="list-of-profiles" id="followees">
-  <h1> Your <br/> Followees</h1>
-  <ul>
-    <li v-for="author in followees.items" :key="author.id">
-      <template v-if="author">
-      <img :src="author.profileImage">
-      <p>{{displayUsername(author.displayName)}}</p>
+  <div class="list-of-profiles" id="followees">
+    <h1> Your <br/> Followees</h1>
+    <ul>
+      <li v-for="author in followees.items" :key="author.url">
+        <template v-if="author">
+        <img :src="author.profileImage">
+        <p>{{displayUsername(author.displayName)}}</p>
+      </template>
+      </li>
+    </ul>
+  </div>
     </template>
-    </li>
-  </ul>
-</div>
-  </template>
-
-<script>
-import { useUserStore } from '@/stores/user'
-import { mapStores } from 'pinia'
-import axios from 'axios'
-
-export default {
-  data () {
-    return {
-      followees: [''],
-      author: null
-    }
-  },
-  computed: {
-    ...mapStores(useUserStore)
-  },
-  methods: {
-    displayUsername (username) {
-      return '@' + username
+  
+  <script>
+  import { useUserStore } from '@/stores/user'
+  import { mapStores } from 'pinia'
+  import axios from 'axios'
+  
+  export default {
+    data () {
+      return {
+        followees: [''],
+        author: null
+      }
     },
-    getAuthorFromStore () {
-      const userStore = this.userStore
-      userStore.initializeStore()
-      this.author = userStore.user.author
+    computed: {
+      ...mapStores(useUserStore)
     },
-    getFollowees () {
-      axios
-        .get(`/authors/${this.author.id}/following/`)
-        .then((res) => {
-          this.followees = res.data
-          console.log(this.followees)
-        })
-        .catch((err) => {
-          console.log("Couldn't get followees!")
-          console.log(err)
-        })
+    methods: {
+      displayUsername (username) {
+        return '@' + username
+      },
+      getAuthorFromStore () {
+        const userStore = this.userStore
+        userStore.initializeStore()
+        this.author = userStore.user.author
+      },
+      getFollowees () {
+        axios
+          .get(`/authors/${this.author._id}/following/`)
+          .then((res) => {
+            this.followees = res.data
+            console.log(this.followees)
+          })
+          .catch((err) => {
+            console.log("Couldn't get followees!")
+            console.log(err)
+          })
+      }
+    },
+    mounted () {
+      this.getAuthorFromStore()
+      this.getFollowees()
     }
-  },
-  mounted () {
-    this.getAuthorFromStore()
-    this.getFollowees()
   }
-}
-
-</script>
-
-  <style scoped>
-  /* list items that are direct children of unordered list*/
-  .list-of-profiles ul>li{
-    display: inline-block
-  }
-  /* p tags that are children of the class list of profiles */
-  .list-of-profiles p{
-    margin: 10px;
-  }
-
-  img {
-    width: 100px;
-    height: 100px;
-    margin: 10px;
-  }
-  </style>
+  
+  </script>
+  
+    <style>
+    /* list items that are direct children of unordered list*/
+    .list-of-profiles ul>li{
+      display: inline-block
+    }
+    /* p tags that are children of the class list of profiles */
+    .list-of-profiles p{
+      margin: 10px;
+    }
+  
+    img {
+      width: 100px;
+      height: 100px;
+      margin: 10px;
+    }
+    </style>
--- src/components/socialComponents/MyFollowers.vue
+++ src/components/socialComponents/MyFollowers.vue
@@ -1,75 +1,75 @@
 <template>
-<div class="list-of-profiles" id="followers">
-  <h1 id="followers"> Your <br/> Followers</h1>
-  <ul>
-    <li v-for="author in followers.items" :key="author.id">
-      <template v-if="author">
-      <img :src="author.profileImage">
-      <p>{{displayUsername(author.displayName)}}</p>
-      </template>
-    </li>
-  </ul>
-</div>
-  </template>
-
-<script>
-import { useUserStore } from '@/stores/user'
-import { mapStores } from 'pinia'
-import axios from 'axios'
-
-export default {
-  data () {
-    return {
-      followers: [''],
-      author: null
-    }
-  },
-  computed: {
-    ...mapStores(useUserStore)
-  },
-  methods: {
-    displayUsername (username) {
-      return '@' + username
+  <div class="list-of-profiles" id="followers">
+    <h1 id="followers"> Your <br/> Followers</h1>
+    <ul>
+      <li v-for="author in followers.items" :key="author.url">
+        <template v-if="author">
+        <img :src="author.profileImage">
+        <p>{{displayUsername(author.displayName)}}</p>
+        </template>
+      </li>
+    </ul>
+  </div>
+    </template>
+  
+  <script>
+  import { useUserStore } from '@/stores/user'
+  import { mapStores } from 'pinia'
+  import axios from 'axios'
+  
+  export default {
+    data () {
+      return {
+        followers: [''],
+        author: null
+      }
     },
-    getAuthorFromStore () {
-      const userStore = this.userStore
-      userStore.initializeStore()
-      this.author = userStore.user.author
+    computed: {
+      ...mapStores(useUserStore)
     },
-    getFollowers () {
-      axios
-        .get(`/authors/${this.author.id}/followers/`)
-        .then((res) => {
-          this.followers = res.data
-          console.log(this.followers)
-        })
-        .catch((err) => {
-          console.log("Couldn't get followers!")
-          console.log(err)
-        })
+    methods: {
+      displayUsername (username) {
+        return '@' + username
+      },
+      getAuthorFromStore () {
+        const userStore = this.userStore
+        userStore.initializeStore()
+        this.author = userStore.user.author
+      },
+      getFollowers () {
+        axios
+          .get(`/authors/${this.author._id}/followers/`)
+          .then((res) => {
+            this.followers = res.data
+            console.log(this.followers)
+          })
+          .catch((err) => {
+            console.log("Couldn't get followers!")
+            console.log(err)
+          })
+      }
+    },
+    mounted () {
+      this.getAuthorFromStore()
+      this.getFollowers()
     }
-  },
-  mounted () {
-    this.getAuthorFromStore()
-    this.getFollowers()
   }
-}
-
-</script>
-
-<style scoped>
-
-  .list-of-profiles ul>li{
-    display: inline-block
-  }
-
-  .list-of-profiles p{
-    margin: 10px;
-  }
-
-  img {
-    width: 100px;
-    height: 100px;
-    margin: 10px;
-  }
-</style>
+  
+  </script>
+  
+  <style>
+  
+    .list-of-profiles ul>li{
+      display: inline-block
+    }
+  
+    .list-of-profiles p{
+      margin: 10px;
+    }
+  
+    img {
+      width: 100px;
+      height: 100px;
+      margin: 10px;
+    }
+  </style>
