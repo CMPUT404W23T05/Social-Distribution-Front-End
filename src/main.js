596c4114e670d8c9aed47e72152ea24c46d47a3d
jd-lo <jdlo@ualberta.ca>
2023-03-31 07:45:28 -0600

    Created and added axios -- not working

--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import axios from 'axios'
+import axiosUtil from './plugins/axiosUtil.js'
 
 axios.defaults.baseURL = 'https://social-t30.herokuapp.com/api' // API URL
 
 const app = createApp(App)
+app.use(axiosUtil)
 app.use(router, axios).use(createPinia()).mount('#app')
+
 app.config.globalProperties.$http = axios
---
+++ src/plugins/axiosUtil.js
@@ -0,0 +1,63 @@
+import axios from 'axios'
+
+// These are in app; they can be used just like you would with axios, but with this.$[nodename] instead of "axios"
+
+// e.g to get all of our local posts:
+// this.$localNode.get(/authors/${authorid}/$posts)
+
+const localNode = axios.create({
+  baseURL: 'https://social-t30.herokuapp.com/api/',
+  headers: {
+    Authorization: 'Token itQmiFiX.HVJyCDX61EIHHjWRYm5naspa204iMrQA'
+  }
+})
+
+const node7 = axios.create({
+  baseURL: 'https://sd7-api.herokuapp.com/api/',
+  headers: {
+    Authorization: `Basic ${btoa('node01:P*ssw0rd!')}`
+  }
+})
+
+const node10 = axios.create({
+  baseURL: 'https://socialdistcmput404.herokuapp.com/api/',
+  headers: {
+    Authorization: 'Token d960c3dee9855f5f5df8207ce1cba7fc1876fedf'
+  }
+})
+
+// These are not configured in app; import them as you need them per component.
+// e.g in <script>: import {queryAllNodes} from 'axiosUtil.js'
+
+function retrieveCredentials (endpoint) {
+  // In case we don't want to store the credentials on front-end (Probably a good idea)
+}
+
+// To use this,
+async function queryAllNodes (method = 'get', endpoint, params = null, data = null) {
+  // method is axios.get/post/put (a function)
+  // endpoint is the part the succeeds the base url e.g '/authors/4/posts/'
+  // params is additional queries. These are optional; ignored if null
+  // data is the JS object that is to be passed to server (if any)
+
+  // Append more nodes here
+  const nodes = [localNode, node7, node10]
+
+  const responses = {}
+
+  for (const node of nodes) {
+    responses[node] = await node[method](endpoint, data, params)
+  }
+
+  // Output: {node7: {...}, node10: {...}, ...}
+  return responses
+}
+
+// Makes these globally available
+export default {
+  install (app) {
+    app.config.globalProperties.$localNode = localNode
+    app.config.globalProperties.$node7 = localNode
+    app.config.globalProperties.$node10 = localNode
+  }
+}
