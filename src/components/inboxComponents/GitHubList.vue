715aa9dbde30cc2a18787c358700c23e83f5b22b
geofferb <38046772+geofferb@users.noreply.github.com>
2023-04-04 23:44:07 -0600

    #87: fill out github card footer with user info

--- src/components/inboxComponents/GitHubList.vue
+++ src/components/inboxComponents/GitHubList.vue
@@ -3,6 +3,8 @@
     <div class="notification-list d-flex flex-wrap">
         <!-- Anchor is where to re-direct on click -->
         <GenericCard
+          @hovered="hovered=true"
+          @unhover="hovered=false"
           v-for="event in feed"
           :key="event.id"
           class="m-2"
@@ -15,9 +17,17 @@
 
         </template>
         <template #footer>
-          <h6 class="notification-type">
-           {{ event.type }} : {{ event.repo.url }}
-          </h6>
+          <img class="profile-image ms-2" :src="event.actor.avatar_url" :class="{ open: hovered }" :title="event.actor.display_login + '\'s GitHub image'" />
+          <div class="d-flex align-items-center justify-content-between w-100 h-100 px-3 overflow-hidden">
+
+          <div>
+          {{ event.actor.display_login }}
+          </div>
+          <a :href = "'https://github.com/' + event.actor.login" target="_blank">
+          <i class="bi bi-github" title="Visit GitHub profile"></i>
+            </a>
+        </div>
+
         </template>
 
         </GenericCard>
@@ -61,3 +71,27 @@ export default {
   }
 }
 </script>
+<style scoped>
+ .profile-image {
+    height: 0em;
+    width: 0em;
+    border-radius: 50%;
+    object-fit: cover;
+    transition: 0.4s;
+  }
+  .profile-image.open {
+    height: 5em;
+    width: 5em;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  a:hover {
+    color: #4998f5
+  }
+  .bi-github {
+    font-size: 1.5em;
+  }
+
+</style>
